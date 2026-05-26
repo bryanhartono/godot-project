53d2cf9f940eb6214f3b65a3470393640063f965
Bryan Hartono <bryanhartono03@gmail.com>
2026-05-26 20:50:52 +0800

    reset project

--- scripts/main_menu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _on_play_pressed() -> void:
-	AudioManager.play("ui_click", -6.0)
-	get_tree().change_scene_to_file("res://scenes/lobby.tscn")
-
-func _on_quit_pressed() -> void:
-	AudioManager.play("ui_click", -6.0)
-	get_tree().quit()
